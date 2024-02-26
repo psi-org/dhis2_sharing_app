7114771c9333e05467920fbc3aced43e50a97809
Mario López <00046317@uca.edu.sv>
2024-02-26 11:28:11 -0600

    Code refactor

--- d2.config.js
+++ d2.config.js
@@ -1,9 +1,12 @@
 const config = {
     type: 'app',
-
+    title: 'Sharing Settings',
+    description: "DHIS2 Objects Sharing Management Tool. Apply Sharing Settings in bulk or individually to any DHIS2 object.",
     entryPoints: {
         app: './src/App.js',
     },
+    minDHIS2Version: '2.37',
+    maxDHIS2Version: '2.40'
 }
 
 module.exports = config
